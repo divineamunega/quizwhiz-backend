9c8a5a0e0ab075924fb147c26bee71396bd726ed
divineamunega <divineamunega@gmail.com>
2024-07-27 23:27:04 +0100

    =feat:Sign up

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,38 @@
-import { validationResult } from "express-validator";
+import bycrypt from "bcryptjs";
+import prisma from "../prisma";
 import AsyncErrorHandler from "../errors/AsyncErrorHandler";
+import { signToken } from "../utils/jwt";
 
 const signup = AsyncErrorHandler(async (req, res, next) => {
-	const response = validationResult(req);
-	console.log(response);
-	res.json({ message: "Hello" });
+	const { name, email, password } = req.data;
+
+	// Hash the passwords here
+	const hashedPassword = await bycrypt.hash(password, 12);
+
+	// Create new user in the database
+	const newUser = await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	// Sign A Json Web Token
+	const token = signToken(newUser.id);
+
+	// Send back user data + JWT to client
+	res.status(201).json({
+		status: "success",
+		token,
+		data: {
+			user: {
+				id: newUser.id,
+				name: newUser.name,
+				email: newUser.email,
+			},
+		},
+	});
 });
 
 const login = AsyncErrorHandler(async (req, res, next) => {});
