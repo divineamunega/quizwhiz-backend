ef220e4293d7e179f732278a330a80699893d1fe
divineamunega <divineamunega@gmail.com>
2024-07-31 00:02:23 +0100

    modified:   src/controllers/QuizController.ts 🚀

--- src/controllers/QuizController.ts
+++ src/controllers/QuizController.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from "express";
 import prisma from "../prisma";
 import AsyncErrorHandler from "../errors/AsyncErrorHandler";
+import AppError from "../errors/AppError";
 
 // JUst testing randome stuff
 const createQuiz = AsyncErrorHandler(async function (
@@ -37,36 +38,48 @@ const createQuestions = AsyncErrorHandler(async (req, res, next) => {
 	const userId = req.user?.id;
 	const data = req.data;
 
-	const newQuestion = await prisma.$transaction(async (prisma) => {
-		const newQuestion = prisma.question.create({
-			data: {
-				question: data.question,
-				quizId: quizId,
-
-				answers: {
-					create: data.answers.map((answer: any) => {
-						return { answer: answer.answer, isCorrect: answer.isCorrect };
-					}),
-				},
-			},
-		});
-
-		// UPDATE QUIZ
-		await prisma.quiz.update({
-			where: { id: quizId },
-			data: {
-				numberOfQuestions: {
-					increment: 1,
-				},
-			},
-		});
-
-		return newQuestion;
+	console.log(data);
+	const quiz = await prisma.quiz.findFirst({
+		where: { id: quizId, creatorId: userId },
 	});
 
+	if (!quiz)
+		throw new AppError(
+			`This user does not have any quiz with the id ${quizId}.`,
+			404
+		);
+
+	const { question, answers } = await prisma.$transaction(async (prisma) => {
+		const question = await prisma.question.create({
+			data: { question: data.question, quizId: quizId },
+		});
+
+		await prisma.answer.createMany({
+			data: [
+				...data.answers.map((obj: any) => {
+					return {
+						answer: obj.answer,
+						isCorrect: obj.isCorrect,
+						questionId: question.id,
+					};
+				}),
+			],
+		});
+
+		const answers = await prisma.answer.findMany({
+			where: { questionId: question.id },
+		});
+		return { question, answers };
+	});
+
+	console.log(answers);
+
 	res.status(200).json({
-		newQuestion,
-		userId,
+		id: question.id,
+		question: question.question,
+		answers: answers.map((answer) => {
+			return { answer: answer.answer, isCorrect: answer.isCorrect };
+		}),
 	});
 });
 
