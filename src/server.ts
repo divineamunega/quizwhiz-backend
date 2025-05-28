df70578c6db06efa33cd6da29bfaf63e3a6a5e45
divineamunega <divineamunega@gmail.com>
2025-05-28 16:03:12 +0100

    fix: env issue

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import "module-alias/register";
-
-import { configDotenv } from "dotenv";
-configDotenv({ path: "./.env" });
-
 import app from "./app";
 import { createServer } from "node:http";
 
 const PORT = process.env.PORT || 4000;
 
 const server = createServer(app);
-
-console.log(process.env.NODE_ENV);
 
 server.listen(PORT, () => {
 	console.log(
