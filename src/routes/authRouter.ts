a022bd3c83101a891160a06931c419dbff2e3185
divineamunega <divineamunega@gmail.com>
2025-07-05 14:49:24 +0100

    fiix: spelling errors with routes

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -14,7 +14,7 @@ const router = Router({ caseSensitive: false });
 router.post("/signup", signupValidator(), signup);
 router.post("/login", loginValidator(), login);
 router.get("/verify_email", protect, verifyEmail);
-router.get("/resend-verfication", protect, resendVerification);
+router.get("/resend-verification", protect, resendVerification);
 router.get("/refresh", refresh);
 
 export { router as authRouter };
