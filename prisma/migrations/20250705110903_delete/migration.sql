158ec8629195f39a0b497459edbb8186679d93e8
divineamunega <divineamunega@gmail.com>
2025-07-05 13:47:20 +0100

    feat: remove quiz

---
+++ prisma/migrations/20250705110903_delete/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Quiz" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
--- src/controllers/quiz/index.ts
+++ src/controllers/quiz/index.ts
@@ -1,3 +1,4 @@
 export * from "./getQuiz";
 export * from "./createQuiz";
 export * from "./addQuestion";
+export * from "./removeQuiz";
